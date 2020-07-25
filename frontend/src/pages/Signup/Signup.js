41264042a16fc9c1bd95e11af2354e2a0d87d934
khalea <khaleaberry@gmail.com>
2020-07-25 16:55:47 -0400

    Form updates

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -3,48 +3,94 @@ import Nav from "../../components/Nav/Nav.js";
 
 import { withRouter } from "react-router";
 
-function Signup() {
-  return (
-    <div>
+// Potentially uodate to use Formik https://formik.org/
+
+class Signup extends React.Component {
+  
+  // Constructor -> Establish relevant state variables 
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.submitForm = this.submitForm.bind(this)
+  }
+
+  // Handle Form Updates
+  handleChange(event) {
+    const value = event.target.value
+    // Matches form name props to state property keys
+    this.setState({ 
+      [event.target.name]: value 
+    })
+    // Prints out current field value
+    console.log("Value:" + event.target.value)
+  }
+
+  // Submit Form
+  submitForm(event) {
+    console.log('Submitted Username: ' + this.state.username + " Password: " + this.state.password)
+    event.preventDefault()
+  }
+
+  // Component
+  render() {
+    return(
+      <div>
       <Nav />
 
+      {/* Form */}
       <div className="login-container flex flex-row justify-center">
-        <form className="login-card flex flex-col justify-center shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form className="login-card flex flex-col justify-center shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={this.submitForm}>
+          
+          {/* Username Field */}
           <label className="font-bold">Username</label>
           <input
-            id="username"
-            type="text"
-            placeholder="Username"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          ></input>
+            value={this.state.username}
+            name="username"
+            onChange={this.handleChange}
 
+            type="text"
+            placeholder="username"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></input>
+
+          {/* Password Field */}
           <label className="font-bold">Password</label>
           <input
-            id="password"
+            value={this.state.password}
+            name="password"
+            onChange={this.handleChange}
+            
             type="text"
-            placeholder="******"
+            placeholder="*********"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           ></input>
 
+          {/* Submit */}
           <div className="flex items-center justify-between">
             <button
               className="bg-hammrBlue hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
+              type="submit">
               Sign In
             </button>
 
-            <a
-              className="inline-block align-baseline font-bold text-sm text-hammrBlue hover:text-blue-600"
-              href="#"
-            >
-              Forgot Password?
-            </a>
+            
+
+            {/* Forgot Password */}            
+            <a className="inline-block align-baseline font-bold text-sm text-hammrBlue hover:text-blue-600" href="#">
+              Forgot Password? </a>
           </div>
         </form>
       </div>
     </div>
-  );
+    )
+  }
+
+
 }
 
 export default withRouter(Signup);
