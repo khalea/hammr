169bfda039fa0f183041d7726a84fb46f06e42e6
khalea <khaleaberry@gmail.com>
2020-07-26 21:51:48 -0400

    minor request update

---
+++ api/README.md
@@ -0,0 +1,8 @@
+# hammr API
+
+### Resources & References
+
+Here's some things that are helping me along the way.
+
+- Kati Frantz: [React Voter App Tutorial](https://katifrantz.com/watch/build-a-scalable-voting-application-with-nodejs-and-reactjs/setting-up-the-backend-server)
+- [Create React App](https://create-react-app.dev/)
--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const bodyParser  = require('body-parser');
 const port = 3001
 
 // Run node src/index.js to start
 
+// Parse URLencoded files
+app.use(bodyParser.urlencoded({extended: true}))
+// Parse JSON body
+app.use(bodyParser.json())
+
 app.get('/api/', (req, res) => res.send('API works'))
-app.get('/api/hello/', (req, res) => res.send('Hello World!'))
+// app.get('/api/hello/', (req, res) => {res.send('Hello World!')})
+
+app.post('/api/hello/', (req, res) => {
+    console.log(req.body.user)
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
